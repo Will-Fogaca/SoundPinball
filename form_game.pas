b9105d871865f5dd50158456e80f451446183a99
= <=>
2024-08-07 14:44:33 -0300

    Upload com form_game

---
+++ form_game.pas
@@ -0,0 +1,24 @@
+unit form_game;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  Tform_gaem = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  form_gaem: Tform_gaem;
+
+implementation
+
+{$R *.fmx}
+
+end.
